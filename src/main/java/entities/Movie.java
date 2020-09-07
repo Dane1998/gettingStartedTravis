0dd921fcff095f904f2df77c2b890b40827d07a4
Daniel <daniel.poulsen@live.dk>
2020-09-07 17:10:24 +0200

    fejl rettelser

---
+++ src/main/java/entities/Movie.java
@@ -0,0 +1,65 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "RenameMe.deleteAllRows", query = "DELETE from RenameMe")
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int year;
+    private String title;
+    private String[] actors;
+
+    public Movie() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getActors() {
+        return actors;
+    }
+
+    public void setActors(String[] actors) {
+        this.actors = actors;
+    }
+
+    public Movie(int year, String title, String[] actors) {
+        this.year = year;
+        this.title = title;
+        this.actors = actors;
+    }
+
+    // TODO, delete this class, or rename to an Entity class that makes sense for what you are about to do
+    // Delete EVERYTHING below if you decide to use this class, it's dummy data used for the initial demo
+}
